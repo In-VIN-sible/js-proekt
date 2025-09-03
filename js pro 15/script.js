8d10acc56293798fd7e72930f98722b0e4bb46e5
Illia0I0 <illiaregent2011@gmail.com>
2025-09-03 22:41:55 +0400

    Update script.js

---
+++ js pro 15/script.js
@@ -0,0 +1,23 @@
+ async function loadUsers() {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+
+        const app = document.getElementById("app");
+
+        users.forEach(user => {
+          const div = document.createElement("div");
+          div.className = "user";
+          div.innerHTML = `
+            <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Місто:</b> ${user.address.city}</p>
+          `;
+          app.appendChild(div);
+        });
+      } catch (error) {
+        console.error("Помилка при завантаженні користувачів:", error);
+      }
+    }
+
+    loadUsers();
